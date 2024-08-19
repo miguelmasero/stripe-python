46dfa1c61e4c0559771c3dbe745f9988a0f30cde
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-08-19 22:37:02 +0000

    Update generated code for v1208

--- stripe/_invoice.py
+++ stripe/_invoice.py
@@ -2450,7 +2450,7 @@ class Invoice(
         """
         unit_amount: NotRequired[int]
         """
-        A positive integer in cents (or local equivalent) (or 0 for a free price) representing how much to charge.
+        A positive integer in cents (or local equivalent) (or 0 for a free price) representing how much to charge or a negative integer representing the amount to credit to the customer.
         """
         unit_amount_decimal: NotRequired[str]
         """
@@ -4287,7 +4287,7 @@ class Invoice(
         """
         unit_amount: NotRequired[int]
         """
-        A positive integer in cents (or local equivalent) (or 0 for a free price) representing how much to charge.
+        A positive integer in cents (or local equivalent) (or 0 for a free price) representing how much to charge or a negative integer representing the amount to credit to the customer.
         """
         unit_amount_decimal: NotRequired[str]
         """
@@ -5411,7 +5411,7 @@ class Invoice(
         """
         unit_amount: NotRequired[int]
         """
-        A positive integer in cents (or local equivalent) (or 0 for a free price) representing how much to charge.
+        A positive integer in cents (or local equivalent) (or 0 for a free price) representing how much to charge or a negative integer representing the amount to credit to the customer.
         """
         unit_amount_decimal: NotRequired[str]
         """
